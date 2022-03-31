a8c29ad8f5b2934e838b4bee5835bbe540b702d2
kaifraza28 <itzkaifraza@gmail.com>
2022-03-31 15:48:29 +0530

    FlipKart Test

---
+++ flipkart.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+product = input("Please enter you item to be searched\n")
+driver = webdriver.Chrome(ChromeDriverManager().install())
+print("Test Case Started")
+driver.maximize_window()
+driver.get("https://www.flipkart.com/")
+time.sleep(2)
+driver.find_element_by_xpath("/html/body/div[2]/div/div/button").click()
+time.sleep(2)
+driver.find_element_by_name("q").send_keys(product)
+time.sleep(1)
+driver.find_element_by_class_name("L0Z3Pu").click()
+time.sleep(5)
+driver.close()
+print("Test cases has successfully completed")
--- test_flipkart.py
+++ test_flipkart.py
@@ -1,17 +1,22 @@
+import pytest
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-product = input("Please enter you item to be searched\n")
-driver = webdriver.Chrome(ChromeDriverManager().install())
-print("Test Case Started")
-driver.maximize_window()
-driver.get("https://www.flipkart.com/")
-time.sleep(2)
-driver.find_element_by_xpath("/html/body/div[2]/div/div/button").click()
-time.sleep(2)
-driver.find_element_by_name("q").send_keys(product)
-time.sleep(1)
-driver.find_element_by_class_name("L0Z3Pu").click()
-time.sleep(5)
-driver.close()
-print("Test cases has successfully completed")
+@pytest.fixture
+def setUp():
+    global product,driver
+    product=input("Pleasee enter product to be search:")
+    driver=webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield
+    time.sleep(5)
+    driver.close()
+
+def test_searchProduct(setUp):
+    driver.get("https://www.flipkart.com/")
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div[2]/div/div/button").click()
+    time.sleep(1)
+    driver.find_element_by_name("q").send_keys(product)
+    time.sleep(1)
+    driver.find_element_by_class_name("L0Z3Pu").click()
